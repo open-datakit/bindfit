a32ded7f41f1440d499c872688d5bf47a3c4a701
echus <varvara@echus.co>
2023-11-01 10:22:44 +1100

    Add opendatafit library dependency, and pre-commit to flake

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   description = "";
 
-  # Use the unstable nixpkgs to use the latest set of node packages
+  # Use the unstable nixpkgs to use the latest set of Python packages
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/master";
 
   outputs = {
@@ -14,10 +14,33 @@
       pkgs = import nixpkgs {
         inherit system;
       };
+
+      opendatafit = pkgs.python3Packages.buildPythonPackage rec {
+        name = "opendatafit";
+        version = "db7b0801336f7011c2f14317cae499965df528b0";
+        format = "pyproject";
+
+        src = pkgs.fetchFromGitHub {
+           owner = "opendatafit";
+           repo = "${name}";
+           rev = "${version}";
+           sha256 = "12v79gwp7kr1di53kwfq09wsmp5mzh3dd58zx221slb6sdcgnnjr";
+        };
+
+        buildInputs = with pkgs.python3Packages; [
+          setuptools
+        ];
+
+        propagatedBuildInputs = with pkgs.python3Packages; [
+          pandas
+        ];
+      };
+
     in {
       devShells.default = pkgs.mkShell {
         buildInputs = [
-          # Set the major version of Node.js
+          opendatafit
+          pkgs.pre-commit
           pkgs.python310
           pkgs.python310Packages.numpy
           pkgs.python310Packages.scipy
