c46467df2b30bb044fd5131df66a0d6c717a4ba6
echus <varvara@echus.co>
2023-11-14 16:25:14 +1100

    Add NMR/UV dimer and coek agg fitters

--- bindfit/datapackage.py
+++ bindfit/datapackage.py
@@ -19,9 +19,13 @@ MODEL_COEFFS_MAP = {
     "nmr1to1": ["H", "HG"],
     "nmr1to2": ["H", "HG", "HG2"],
     "nmr2to1": ["H", "HG", "H2G"],
+    "nmrdimer": ["H", "Hs", "He"],
+    "nmrcoek": ["H", "Hs", "He"],
     "uv1to1": ["H", "HG"],
     "uv1to2": ["H", "HG", "HG2"],
     "uv2to1": ["H", "HG", "H2G"],
+    "uvdimer": ["H", "Hs", "He"],
+    "uvcoek": ["H", "Hs", "He"],
 }
 
 
