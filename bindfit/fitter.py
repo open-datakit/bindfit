4501b917d1bef887e24f4c07c5265b5ec7e59c6f
Varvara Efremova <varvara@echus.co>
2024-07-04 19:40:42 +1000

    Add fit quality property (#15)

--- bindfit/fitter.py
+++ bindfit/fitter.py
@@ -431,12 +431,15 @@ class Fitter:
         """Return optimised coefficients table as pandas DataFrame"""
         return (
             pd.DataFrame(np.transpose(self.coeffs))
+            # Set index to fit column names
             .set_index(self.data.columns.rename("name"))
+            # Set coefficient column names
             .set_axis(self.MODEL_COEFFS_MAP[self.function.f.__name__], axis=1)
         )
 
     @property
     def fit_summary(self):
+        """Return fit summary data as pandas DataFrame"""
         return pd.DataFrame(
             [
                 [
@@ -455,3 +458,22 @@ class Fitter:
                 "Fitted parameters",
             ],
         ).set_index("Model")
+
+    @property
+    def fit_quality(self):
+        """Return fit quality statistics as pandas DataFrame"""
+        return (
+            pd.DataFrame(
+                np.transpose(
+                    [
+                        helpers.rms(self.residuals),
+                        helpers.cov(self.ydata, self.residuals),
+                    ]
+                ),
+                columns=["RMS", "Covariance"],
+            )
+            # Set index to fit column names
+            # Doing it this way instead of setting index in constructor as
+            # this allows us to use a custom named index
+            .set_index(self.data.columns.rename("Fit"))
+        )
