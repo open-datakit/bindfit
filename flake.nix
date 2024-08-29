888f507b79eb6b18e843bbe3b753295a4d99d545
Varvara Efremova <varvara@echus.co>
2024-08-29 18:35:24 +1000

    Remove unused opendatafit/py library dependency

--- flake.nix
+++ flake.nix
@@ -14,32 +14,9 @@
       pkgs = import nixpkgs {
         inherit system;
       };
-
-      opendatafit = pkgs.python3Packages.buildPythonPackage rec {
-        name = "opendatafit";
-        version = "db7b0801336f7011c2f14317cae499965df528b0";
-        format = "pyproject";
-
-        src = pkgs.fetchFromGitHub {
-           owner = "opendatafit";
-           repo = "${name}";
-           rev = "${version}";
-           sha256 = "12v79gwp7kr1di53kwfq09wsmp5mzh3dd58zx221slb6sdcgnnjr";
-        };
-
-        buildInputs = with pkgs.python3Packages; [
-          setuptools
-        ];
-
-        propagatedBuildInputs = with pkgs.python3Packages; [
-          pandas
-        ];
-      };
-
     in {
       devShells.default = pkgs.mkShell {
         buildInputs = [
-          opendatafit
           pkgs.pre-commit
           pkgs.python311
           pkgs.python311Packages.numpy
